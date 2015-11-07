d8bdc15b4b2ed42f0f1d5be7c199927a44bcf2a3
marsette25 <marsie.thomas@gmail.com>
2015-11-07 11:01:26 -0600

    first push

--- Brello/Models/BoardContext.cs
+++ Brello/Models/BoardContext.cs
@@ -16,7 +16,7 @@ namespace Brello.Models
 
         public virtual IDbSet<Color> Colors { get; set; }
         public virtual IDbSet<Card> Cards { get; set; }
-        public virtual IDbSet<Board> Boards { get; set; }
+        public virtual DbSet<Board> Boards { get; set; }
 
         // IQueryable, IList/List, ICollection/Collection
     }
